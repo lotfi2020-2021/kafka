b5f1a8ea25696f3bb1ef07ef28846a1a27a1f70b
lotfi2020-2021 <lotfi.hmida@esprit.tn>
2023-08-09 18:58:27 +0100

    kafka

---
+++ kafka-consumer/src/main/java/com/notification/service/kafkaconsumer/model/Notification.java
@@ -0,0 +1,16 @@
+package com.notification.service.kafkaconsumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+   private Long id;
+   private String content;
+   private NotificationType type;
+}
